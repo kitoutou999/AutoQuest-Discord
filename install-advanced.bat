f976f77f9d5c283cdc845525d315723c76946e01
DavidTom <tomdavid09022003@gmail.com>
2025-10-25 18:35:58 +0200

    before cleanUp

--- install-advanced.bat
+++ install-advanced.bat
@@ -1,11 +1,14 @@
 @echo off
-echo ========================================
-echo Discord PTB Custom Button - Installation Avancee
-echo (Modification du core.asar)
-echo ========================================
+
+
+
 echo.
 node install-asar.js uninstall
+
+echo.
 node install-asar.js install
 
 echo.
+start "" "%LOCALAPPDATA%\DiscordPTB\Update.exe" --processStart DiscordPTB.exe
+
 
